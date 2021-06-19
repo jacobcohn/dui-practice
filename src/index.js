1cf5dbfc48b864824d98a2b4840a60d25643f305
Jacob Cohn <jacobnc916@gmail.com>
2021-06-19 12:37:25 -0500

    organized index.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,13 @@
 import createDropDownMenu from './createDropDownMenu';
-import events from './events';
 
 /*
 Things that I need to do:
 - initiate
 - when a menuItem is clicked
 - sources button is clicked
-
  */
 
-const main = (() => {
+const dropDownMenu = (() => {
   const dropDownMenuDiv = document.getElementById('dropDownMenuDiv');
 
   const menuItems = [
@@ -20,7 +18,40 @@ const main = (() => {
 
   const initiate = () => {
     dropDownMenuDiv.appendChild(createDropDownMenu.initiate(menuItems));
-    events.initiate();
+  };
+
+  return { initiate };
+})();
+
+const imageSlider = (() => {
+  const changeImageSliderTitle = (title) => {
+    const imageSliderTitle = document.getElementById('imageSliderTitleContent');
+    imageSliderTitle.textContent = title;
+  };
+
+  const addEventListenerToMenuItem = (menuItem) => {
+    menuItem.addEventListener('click', () => {
+      changeImageSliderTitle(menuItem.value);
+      // changeImageSlider(menuItem.value);
+    });
+  };
+
+  const findAndCallFunctionForAllMenuItems = () => {
+    const allMenuItems = document.querySelectorAll('.menuItemDiv');
+    allMenuItems.forEach((menuItem) => addEventListenerToMenuItem(menuItem));
+  };
+
+  const initiate = () => {
+    findAndCallFunctionForAllMenuItems();
+  };
+
+  return { initiate };
+})();
+
+const main = (() => {
+  const initiate = () => {
+    dropDownMenu.initiate();
+    imageSlider.initiate();
   };
 
   return { initiate };
